80b9962c244c6c33f1d85de1563ef687aa694fc3
Shem Tom <63319003+shemtomke@users.noreply.github.com>
2022-11-01 09:10:28 +0300

    details

--- app/src/main/java/com/shemtom/enteryourdetails/MainActivity.java
+++ app/src/main/java/com/shemtom/enteryourdetails/MainActivity.java
@@ -1,21 +1,32 @@
 package com.shemtom.enteryourdetails;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText nameTxt, emailTxt, dateBirth;
-    TextView details, topText;
-    Button nextBtn, exitBtn, backBtn;
-    View panel;
-    ConstraintLayout constraintLayout;
+    Button nextBtn, exitBtn;
+
+    AlertDialog dialog;
+    DatePickerDialog.OnDateSetListener dateDialog;
+
+    final Calendar myCalendar= Calendar.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,50 +37,13 @@ public class MainActivity extends AppCompatActivity {
         emailTxt = (EditText) findViewById(R.id.emailTxt); //email
         dateBirth = (EditText) findViewById(R.id.editTextDate); //date
 
-        details = (TextView) findViewById(R.id.detailsDisplayTxt);
-        topText = (TextView) findViewById(R.id.topViewTxt);
-
         nextBtn = (Button) findViewById(R.id.nextButton);
         exitBtn = (Button) findViewById(R.id.ExitBtn);
-        backBtn = (Button) findViewById(R.id.backButton);
 
-        panel = (View) findViewById(R.id.panelView);
-
-        try {
-            constraintLayout.setEnabled(false);
-        }
-        catch (Exception e)
-        {
-
-        }
-
-        //disable the panel, backButton, TopTex and details
-        DisablePanel();
-
+        showDate();
+        showDateDetails();
         Next();
-        Back();
         Exit();
-    }
-
-    void EnablePanel()
-    {
-        panel.setActivated(true);
-        backBtn.setEnabled(true);
-        topText.setEnabled(true);
-        details.setEnabled(true);
-
-        nextBtn.setEnabled(false);
-        exitBtn.setEnabled(false);
-    }
-    void DisablePanel()
-    {
-        panel.setActivated(false);
-        backBtn.setEnabled(false);
-        topText.setEnabled(false);
-        details.setEnabled(false);
-
-        nextBtn.setEnabled(true);
-        exitBtn.setEnabled(true);
     }
 
     //onclick next then save the details
@@ -78,23 +52,51 @@ public class MainActivity extends AppCompatActivity {
         nextBtn.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
-                //display entered details in an output panel
-                EnablePanel();
-                details.setText(nameTxt + "\t" + emailTxt + "\t" + dateBirth);
+                //display entered details in an output panel - dialog
+                showDetailsDialog(nameTxt.getText() + "\n" + dateBirth.getText() + "\n" + emailTxt.getText());
             }
         });
     }
-    public void Back()
+
+    public void showDetailsDialog(String details)
     {
-        backBtn.setOnClickListener(new View.OnClickListener() {
+        dialog = new AlertDialog.Builder(MainActivity.this)
+                .setTitle("Details entered")
+                .setMessage(details)
+                .setPositiveButton("Back", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                }).create();
+        dialog.show();
+    }
+    public void showDateDetails()
+    {
+        dateDialog = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH,month);
+                myCalendar.set(Calendar.DAY_OF_MONTH,day);
+                String myFormat="dd/MM/yy";
+                SimpleDateFormat dateFormat=new SimpleDateFormat(myFormat, Locale.UK);
+                dateBirth.setText(dateFormat.format(myCalendar.getTime()));
+            }
+        };
+    }
+
+    public void showDate()
+    {
+        dateBirth.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
-                //display entered details in an output panel
-                DisablePanel();
-                details.setText("");
+                //display entered details in an output panel - dialog
+                new DatePickerDialog(MainActivity.this,dateDialog,myCalendar.get(Calendar.YEAR),myCalendar.get(Calendar.MONTH),myCalendar.get(Calendar.DAY_OF_MONTH)).show();
             }
         });
     }
+
     //on click exit then exit app
     public void Exit()
     {
